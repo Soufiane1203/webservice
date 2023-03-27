0f3bdd849f5feaece8b86a7ff200670ae142763d
Soufiane1203 <78510802+Soufiane1203@users.noreply.github.com>
2023-03-28 00:14:56 +0200

    Update MovieService.java

--- src/main/java/com.example.movielist/service/MovieService.java
+++ src/main/java/com.example.movielist/service/MovieService.java
@@ -7,9 +7,14 @@ import com.example.movielist.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Service
+@Path("/movies")
 public class MovieService {
     private final MovieRepository movieRepository;
     private final ActorRepository actorRepository;
@@ -18,6 +23,19 @@ public class MovieService {
     public MovieService(MovieRepository movieRepository, ActorRepository actorRepository) {
         this.movieRepository = movieRepository;
         this.actorRepository = actorRepository;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Movie> getMovies() {
+        return movieRepository.findAll();
+    }
+
+    @GET
+    @Path("/actors")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Actor> getAllActors() {
+        return actorRepository.findAll();
     }
 
     public Movie addMovie(Movie movie) {

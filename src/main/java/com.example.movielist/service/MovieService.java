847fadbbe5f6853bd48967392588c48a057c6d90
Soufiane1203 <78510802+Soufiane1203@users.noreply.github.com>
2023-03-28 01:13:34 +0200

    Update MovieService.java

--- src/main/java/com.example.movielist/service/MovieService.java
+++ src/main/java/com.example.movielist/service/MovieService.java
@@ -13,9 +13,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Service
-@Path("/movies")
+@Service // denotes this class as a Spring service component
+@Path("/movies") // sets the base path for JAX-RS resource methods
 public class MovieService {
+
+    // injects the necessary repositories via constructor dependency injection
     private final MovieRepository movieRepository;
     private final ActorRepository actorRepository;
 
@@ -25,12 +27,14 @@ public class MovieService {
         this.actorRepository = actorRepository;
     }
 
+    // JAX-RS resource method that returns all movies as JSON
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Movie> getMovies() {
         return movieRepository.findAll();
     }
 
+    // JAX-RS resource method that returns all actors as JSON
     @GET
     @Path("/actors")
     @Produces(MediaType.APPLICATION_JSON)
@@ -38,14 +42,17 @@ public class MovieService {
         return actorRepository.findAll();
     }
 
+    // adds a movie to the database via the movie repository
     public Movie addMovie(Movie movie) {
         return movieRepository.save(movie);
     }
 
+    // adds an actor to the database via the actor repository
     public void addActor(Actor actor) {
         actorRepository.save(actor);
     }
 
+    // links a movie to an actor by adding the actor to the movie's list of actors
     public void linkMovieToActor(Long movieId, Long actorId) {
         Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new IllegalArgumentException("Invalid movie ID"));
         Actor actor = actorRepository.findById(actorId).orElseThrow(() -> new IllegalArgumentException("Invalid actor ID"));
@@ -56,10 +63,12 @@ public class MovieService {
         movieRepository.save(movie);
     }
 
+    // retrieves a movie by its title via the movie repository
     public Movie getMovieByTitle(String title) {
         return movieRepository.findByTitle(title).orElseThrow(() -> new IllegalArgumentException("Invalid movie title"));
     }
 
+    // retrieves all movies that have an actor with a given name via the movie repository
     public List<Movie> getMoviesByActorName(String name) {
         return movieRepository.findByActorsName(name);
     }

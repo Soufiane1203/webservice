f3bdc02d44cfb14e2a8f6934bf91a2dd5e189c03
Soufiane1203 <78510802+Soufiane1203@users.noreply.github.com>
2023-03-28 01:09:32 +0200

    Update ResourceNotFoundException.java

--- src/main/java/com.example.movielist/com.example.movielist.exception/ResourceNotFoundException.java
+++ src/main/java/com.example.movielist/com.example.movielist.exception/ResourceNotFoundException.java
@@ -1,10 +1,14 @@
+// Import necessary libraries
 package com.example.movielist.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// Set the response status to NOT_FOUND
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+    
+    // Constructor with message
     public ResourceNotFoundException(String message) {
         super(message);
     }
